ec28ff05197cf1d4b370fcaa497093cf64330bdd
444dima81 <444dima81dmitriy@gmail.com>
2025-08-13 10:49:19 +0300

    3 commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# steamlit_app
+# Реализация двух проектов
+### 1. Визаулизация Apple котировок на платформе Streamlit
+[# steamlit_app](https://444dima81-steamlit-app-main-v2sn9k.streamlit.app/)
+
+
+### 2. Визаулизация графической статистики файла tips.csv на платформе Streamlit
+[# steamlit_app2](https://444dima81-steamlit-app-main-v2sn9k.streamlit.app/)
--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import streamlit as st
-import yfinance as yf
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-from datetime import date, timedelta
 
 # Настройка оформления
-st.set_page_config(page_title="Tips Dashbords", layout="wide")
+st.set_page_config(page_title="Tips Dashboards", layout="wide")
 sns.set_theme(style="whitegrid")
 tips = pd.read_csv('tips.csv')
 
-# --- Боковая панель ---
+#  Боковая панель
 st.sidebar.header("Настройки фильтрации")
 
 sex_filter = st.sidebar.multiselect(
@@ -35,21 +33,30 @@ chart_type = st.sidebar.selectbox(
     "Тип графика",
     ["Scatter", "Box", "Histogram"]
 )
+
 point_size = st.sidebar.slider("Размер точек (для Scatter)", 50, 300, 100)
+
+# Фунция фильтрации
+tips_filtered = tips[
+    (tips['sex'].isin(sex_filter)) &
+    (tips['smoker'].isin(smoker_filter)) &
+    (tips['time'].isin(time_filter))
+]
+
 # Header
-st.title('Проект по визализации данных')
-st.subheader(f"Подбробная Статистика Чаевых ", divider='gray')
+st.title('Проект по визуализации данных')
+st.subheader("Подробная статистика чаевых", divider='gray')
 
 # Вывод таблицы
-st.markdown("### Общий план данных")
-st.dataframe(tips)
+st.markdown("### Отфильтрованные данные")
+st.dataframe(tips_filtered)
 
-# Построение графиков
+# Построение графиков 
 fig, ax = plt.subplots(figsize=(8, 5))
 
 if chart_type == "Scatter":
     sns.scatterplot(
-        data=tips,
+        data=tips_filtered,
         x='total_bill',
         y='tip',
         hue='sex',
@@ -58,24 +65,30 @@ if chart_type == "Scatter":
         ax=ax
     )
     ax.set_title("Связь между счетом и чаевыми")
+    ax.set_xlabel("Сумма счёта")
+    ax.set_ylabel("Чаевые")
 
 elif chart_type == "Box":
     sns.boxplot(
-        data=tips,
+        data=tips_filtered,
         x='day',
         y='total_bill',
         hue='time',
         ax=ax
     )
     ax.set_title("Сумма счетов по дням недели")
+    ax.set_xlabel("День недели")
+    ax.set_ylabel("Сумма счёта")
 
 elif chart_type == "Histogram":
     sns.histplot(
-        data=tips,
+        data=tips_filtered,
         x='total_bill',
         hue='sex',
         ax=ax
     )
     ax.set_title("Распределение суммы счетов")
+    ax.set_xlabel("Сумма счёта")
+    ax.set_ylabel("Количество заказов")
 
 st.pyplot(fig)
